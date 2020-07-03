963a308af6845f2d6331ed491685f0b6e6bebace
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-02 22:05:24 -0300

    modified teachers path and started added routes for the form in teachers/create.njk

--- routes.js
+++ routes.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get("/", function (req, res) {
+routes.get("/", (req, res) => {
     return res.redirect('/teachers')
 })
 
-routes.get("/teachers", function (req, res) {
-    return res.render('teachers')
+routes.get("/teachers", (req, res) => {
+    return res.render('/teachers/index')
 })
 
-routes.get("/students", function (req, res) {
+routes.get("/teachers/create", (req, res) => {
+    return res.render('/teachers/create')
+})
+
+routes.post("/teachers/create", (req, res) => {
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "")
+            return res.send('please, fill all the fields')
+    }
+
+    return res.render(req.body)
+})
+
+routes.get("/students", (req, res) => {
     return res.send('students')
 })
 

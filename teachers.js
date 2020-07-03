48132cb3a07e4cf0fc97761e3dbacf9df3c262d2
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-03 09:19:09 -0300

    finished challenge 4-3: form and teachers route

--- teachers.js
+++ teachers.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 exports.post = (req, res) => {
     const keys = Object.keys(req.body)
 
@@ -6,5 +9,26 @@ exports.post = (req, res) => {
             return res.send('please, fill all the fields')
     }
 
-    return res.send(req.body)
+    let { avatar_url, name, birth, degree, modality, lessons } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        degree,
+        modality,
+        lessons,
+        created_at
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) return res.send('An error occurred!')
+
+        return res.redirect('/teachers')
+    })
 }
